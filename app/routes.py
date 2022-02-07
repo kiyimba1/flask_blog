d31603cccd0bb74c018f40b6362b05682d0dc8df
kiyimba1 <edwineddie54@gmail.com>
2022-02-07 18:10:01 +0300

    Added pagination to profile pages

--- app/routes.py
+++ app/routes.py
@@ -27,7 +27,11 @@ def index():
         return redirect(url_for('index'))
     page = request.args.get('page', 1, type=int)
     posts = current_user.followed_posts().paginate(page, app.config['POSTS_PER_PAGE'],False)
-    return render_template('index.html', title='Home', form=form, posts=posts.items)
+    next_url = url_for(
+        'index', page=posts.next_num) if posts.has_next else None
+    prev_url = url_for(
+        'index', page=posts.prev_num) if posts.has_prev else None
+    return render_template('index.html', title='Home', form=form, posts=posts.items, next_url=next_url, prev_url=prev_url)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -74,18 +78,14 @@ def register():
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
-    posts = [
-        {
-            'author': user,
-            'body': 'Beautiful day in Portland'
-        },
-        {
-            'author': user,
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
+    page = request.args.get('page', 1, type=int)
+    posts = user.posts.order_by(Post.itemstamp.desc()).paginate(page, app.config['POSTS_PER_PAGE'], False)
+    next_url = url_for('user', username=user.username,
+                       page=posts.next_num) if posts.has_next else None
+    prev_url = url_for('user', username=user.username,
+                       page=posts.prev_num) if posts.has_prev else None
     form = EmptyForm()
-    return render_template('user.html', user=user, posts=posts, form=form)
+    return render_template('user.html', user=user, posts=posts.items, form=form, prev_url=prev_url, next_url=next_url)
 
 
 @app.route('/edit-profile', methods=['GET', 'POST'])
@@ -102,13 +102,6 @@ def edit_profile():
         form.username.data = current_user.username
         form.about_me.data = current_user.about_me
     return render_template('edit_profile.html', title='Edit Profile', form=form)
-
-
-@app.route('/explore')
-@login_required
-def explore():
-    posts = Post.query.order_by(Post.timestamp.desc()).all()
-    return render_template('index.html', title='Explore', posts=posts)
 
 
 @app.route('/follow/<username>', methods=['POST'])
@@ -156,4 +149,8 @@ def unfollow(username):
 def explore():
     page = request.args.get('page', 1, type=int)
     posts = Post.query.order_by(Post.timestamp.desc()).paginate(page, app.config['POSTS_PER_PAGE'], False)
-    return render_template('index.html', title='Explore', posts=posts.items)
+    next_url = url_for(
+        'index', page=posts.next_num) if posts.has_next else None
+    prev_url = url_for(
+        'index', page=posts.prev_num) if posts.has_prev else None
+    return render_template('index.html', title='Explore', posts=posts.items, next_url=next_url, prev_url=prev_url)

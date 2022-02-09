ed8c2e4a7aae2bffdae983fec3ac085016c88f14
kiyimba1 <edwineddie54@gmail.com>
2022-02-09 12:02:57 +0300

    Added bootstrap styles

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
-from flask_bootstrap import Bootstrap
+from flask_bootstrap import Bootstrap5
 from flask_moment import Moment
 from flask_babel import Babel
 
@@ -18,7 +18,7 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
-bootstrap = Bootstrap(app)
+bootstrap = Bootstrap5(app)
 moment = Moment(app)
 babel = Babel(app)
 

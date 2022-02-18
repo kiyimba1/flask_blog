a8ea22c40126fb65d335d1fc086798f6272e1e04
kiyimba1 <edwineddie54@gmail.com>
2022-02-18 10:58:25 +0300

    A better Application structure

--- app/models.py
+++ app/models.py
@@ -1,16 +1,19 @@
 from datetime import datetime
-from app import db, login, app
-from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import UserMixin
 from hashlib import md5
 from time import time
+from flask import current_app
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 import jwt
+from app import db, login
 
 
-followers = db.Table('followers',
+followers = db.Table(
+    'followers',
     db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
-    db.Column('followed_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
 )
+
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -24,10 +27,9 @@ class User(UserMixin, db.Model):
         'User', secondary=followers,
         primaryjoin=(followers.c.follower_id == id),
         secondaryjoin=(followers.c.followed_id == id),
-        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic'
-    )
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
 
-    def __repr__(self) -> str:
+    def __repr__(self):
         return '<User {}>'.format(self.username)
 
     def set_password(self, password):
@@ -38,47 +40,41 @@ class User(UserMixin, db.Model):
 
     def avatar(self, size):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
-        return 'https://www.gravatar.com/avatar/{}?d=identicn&s={}'.format(digest, size)
-    
-    def follow(self,user):
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+            digest, size)
+
+    def follow(self, user):
         if not self.is_following(user):
             self.followed.append(user)
-            
+
     def unfollow(self, user):
         if self.is_following(user):
             self.followed.remove(user)
 
     def is_following(self, user):
         return self.followed.filter(
-            followers.c.followed_id == user.id
-        ).count() > 0
-        
+            followers.c.followed_id == user.id).count() > 0
+
     def followed_posts(self):
-        followed = Post.query.join(followers, (followers.c.followed_id == Post.user_id)).filter(followers.c.follower_id == self.id)
+        followed = Post.query.join(
+            followers, (followers.c.followed_id == Post.user_id)).filter(
+                followers.c.follower_id == self.id)
         own = Post.query.filter_by(user_id=self.id)
         return followed.union(own).order_by(Post.timestamp.desc())
-    
+
     def get_reset_password_token(self, expires_in=600):
-        return jwt.encode({'reset_password': self.id, 'exp': time() + expires_in}, app.config['SECRET_KEY'], algorithms=['HS256']).decode('utf-8')
-    
+        return jwt.encode(
+            {'reset_password': self.id, 'exp': time() + expires_in},
+            current_app.config['SECRET_KEY'], algorithm='HS256')
+
     @staticmethod
     def verify_reset_password_token(token):
         try:
-            id = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])['reset_password']
+            id = jwt.decode(token, current_app.config['SECRET_KEY'],
+                            algorithms=['HS256'])['reset_password']
         except:
             return
         return User.query.get(id)
-    
-    
-
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    body = db.Column(db.String(140))
-    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
-    def __repr__(self) -> str:
-        return '<Post {}>'.format(self.body)
 
 
 @login.user_loader
@@ -86,3 +82,12 @@ def load_user(id):
     return User.query.get(int(id))
 
 
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    language = db.Column(db.String(5))
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
--- tests.py
+++ tests.py
@@ -1,17 +1,27 @@
+#!/usr/bin/env python
 from datetime import datetime, timedelta
 import unittest
-from app import app, db
+from app import create_app, db
 from app.models import User, Post
+from config import Config
+
+
+class TestConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite://'
 
 
 class UserModelCase(unittest.TestCase):
-    def setUp(self) -> None:
-        app.config['SQALCHEMY_DATABASE_URI'] = 'sqlite://'
+    def setUp(self):
+        self.app = create_app(TestConfig)
+        self.app_context = self.app.app_context()
+        self.app_context.push()
         db.create_all()
 
-    def tearDown(self) -> None:
+    def tearDown(self):
         db.session.remove()
         db.drop_all()
+        self.app_context.pop()
 
     def test_password_hashing(self):
         u = User(username='susan')
@@ -19,10 +29,11 @@ class UserModelCase(unittest.TestCase):
         self.assertFalse(u.check_password('dog'))
         self.assertTrue(u.check_password('cat'))
 
-    # def test_avatar(self):
-    #     u = User(username='john', email='john@example.com')
-    #     self.assertEqual(u.avatar(
-    #         128), ('https://www.gravatar.com/avatard4c74594d841139328695756648b6bd6?d=identicon&s=128'))
+    def test_avatar(self):
+        u = User(username='john', email='john@example.com')
+        self.assertEqual(u.avatar(128), ('https://www.gravatar.com/avatar/'
+                                         'd4c74594d841139328695756648b6bd6'
+                                         '?d=identicon&s=128'))
 
     def test_follow(self):
         u1 = User(username='john', email='john@example.com')
@@ -32,6 +43,7 @@ class UserModelCase(unittest.TestCase):
         db.session.commit()
         self.assertEqual(u1.followed.all(), [])
         self.assertEqual(u1.followers.all(), [])
+
         u1.follow(u2)
         db.session.commit()
         self.assertTrue(u1.is_following(u2))
@@ -39,6 +51,7 @@ class UserModelCase(unittest.TestCase):
         self.assertEqual(u1.followed.first().username, 'susan')
         self.assertEqual(u2.followers.count(), 1)
         self.assertEqual(u2.followers.first().username, 'john')
+
         u1.unfollow(u2)
         db.session.commit()
         self.assertFalse(u1.is_following(u2))
@@ -52,6 +65,7 @@ class UserModelCase(unittest.TestCase):
         u3 = User(username='mary', email='mary@example.com')
         u4 = User(username='david', email='david@example.com')
         db.session.add_all([u1, u2, u3, u4])
+
         # create four posts
         now = datetime.utcnow()
         p1 = Post(body="post from john", author=u1,
@@ -64,12 +78,14 @@ class UserModelCase(unittest.TestCase):
                   timestamp=now + timedelta(seconds=2))
         db.session.add_all([p1, p2, p3, p4])
         db.session.commit()
+
         # setup the followers
         u1.follow(u2)  # john follows susan
         u1.follow(u4)  # john follows david
         u2.follow(u3)  # susan follows mary
         u3.follow(u4)  # mary follows david
         db.session.commit()
+
         # check the followed posts of each user
         f1 = u1.followed_posts().all()
         f2 = u2.followed_posts().all()
